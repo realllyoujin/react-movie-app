cc25b9802ce9f2b4080e9f7d6e9044d5ddbc79f9
김유진 <reallllyoujin@gmail.com>
2024-08-24 12:05:00 +0900

    movie-app

--- src/App.js
+++ src/App.js
@@ -1,14 +1,57 @@
+import React from 'react';
+import axios from 'axios';
+import Movie from './Movie'; // Movie 컴포넌트를 사용하려면 import 해야 합니다.
+import "./App.css"
 
-import Button from "./Button";
+class App extends React.Component {
+  state = {
+    isLoading: true,
+    movies: []
+  };
 
+  getMovies = async () => {
+    try {
+      const {
+        data: {
+          data: { movies }
+        }
+      } = await axios.get("https://yts-proxy.now.sh/list_movies.json?sort_by=rating");
+      this.setState({ movies, isLoading: false });
+    } catch (error) {
+      console.error("Failed to fetch movies:", error);
+    }
+  };
 
-function App() {
-  return (
-    <div>
-      <h1>Welcome back!</h1>
-      <Button text={"Continue"}/>
-    </div>
-  );
+  componentDidMount() {
+    this.getMovies();
+  }
+
+  render() {
+    const { isLoading, movies } = this.state;
+    return (
+      <section className="container">
+        {isLoading ? (
+          <div className="loader">
+            <span className="loader_text">Loading...</span>
+          </div>
+        ) : (
+          <div className="movies">
+            {movies.map(movie => (
+              <Movie
+                key={movie.id}
+                id={movie.id}
+                year={movie.year}
+                title={movie.title}
+                summary={movie.summary}
+                poster={movie.medium_cover_image}
+                genres={movie.genres}
+              />
+            ))}
+          </div>
+        )}
+      </section>
+    );
+  }
 }
 
 export default App;
